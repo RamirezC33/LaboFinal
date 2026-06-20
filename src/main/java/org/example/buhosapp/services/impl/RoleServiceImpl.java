299fe28c78d9791e3b1c4b4347c8d19596fb0ec3
Elias8420 <eliaszelaya747@gmail.com>
2026-06-20 11:39:41 -0600

    add: service implementation to create role

--- src/main/java/org/example/buhosapp/services/impl/RoleServiceImpl.java
+++ src/main/java/org/example/buhosapp/services/impl/RoleServiceImpl.java
@@ -1,6 +1,8 @@
 package org.example.buhosapp.services.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.example.buhosapp.common.mappers.RoleMapper;
+import org.example.buhosapp.domain.dtos.request.role.CreateRoleRequest;
 import org.example.buhosapp.repositories.RoleRepository;
 import org.example.buhosapp.services.RoleService;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class RoleServiceImpl implements RoleService {
     private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
+
+    @Override
+    public void createRole(CreateRoleRequest roleRequest) {
+        roleRepository.save(roleMapper.toEntityCreate(roleRequest));
+    }
 }
